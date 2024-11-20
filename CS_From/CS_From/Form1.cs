97971e89be7d720c2992344b102ee493c2d2a025
吉田蓮司 <Ry5224@icloud.com>
2024-11-20 13:53:52 +0900

    課題４

--- CS_From/CS_From/Form1.cs
+++ CS_From/CS_From/Form1.cs
@@ -12,9 +12,49 @@ namespace CS_From
 {
     public partial class Form1 : Form
     {
+        TestLabel _testLabel;
+
+        TestTextBox _testTextBox;
+
         public Form1()
         {
             InitializeComponent();
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                TestButton testButton = new TestButton(this,i, (i % 5) * 100, (i / 5) * 100, 100, 100);
+                Controls.Add(testButton);
+            }
+
+            _testLabel = new TestLabel("らべるです。", 10, 250, 500, 100);
+
+            Controls.Add(_testLabel);
+
+            _testTextBox = new TestTextBox("テキストボックスです", 10, 400, 500, 100);
+
+            Controls.Add(_testTextBox);
+            
+            /*
+            Label label = new Label();
+            label.Location = new Point(30,400);
+            label.Text = "らべるです。";
+            Controls.Add(label);
+            */
+
+        }
+
+        public void LabelTextUpdate(string str)
+        {
+            _testLabel.TextUpdate(str);
+        }
+
+        public string  ButtonLabelReplacement(string str)
+        {
+            string s = _textBox.TextReplacement(str);
+
+            return s;
         }
     }
 }
---
+++ CS_From/CS_From/TestTextBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_From
+{
+    internal class TestTextBox:TextBox
+    {
+        public TestTextBox(string str, int x, int y, int width, int height)
+        {
+
+
+            Text = str;
+
+            Location = new Point(x, y);
+
+            Size = new Size(width, height);
+
+        }
+
+        public string TextReplacement(string str)
+        {
+            string temp = Text;
+
+            Text = str;
+
+            return temp;
+
+
+        }
+
+    }
+}
